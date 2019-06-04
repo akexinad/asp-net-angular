b20db766e76ff533bb54e11f70422ecf22385ff0
Ceccattini <Danny.Ceccattini@lendlease.com>
2019-06-04 13:07:29 +1000

    31. Validation in the API

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
-    {
+    {   
+        // Data Validations
+        // The required tag is to ensure that no empty strings are passed for both username and password
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters!")]
         public string Password { get; set; }
     }
 }
