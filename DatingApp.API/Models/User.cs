513212713da1d85cd49c85c77c5e1a5685caa49c
akexinad <danixeka@gmail.com>
2019-06-05 19:17:38 +1000

    69. Entity Framework Relationships

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -7,7 +7,6 @@ namespace DatingApp.API.Models
     {
         public int Id { get; set; }
         public string Username { get; set; }
-
         // We need to install the password as a password hash and password salt in the form of a 'byte array'.
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
