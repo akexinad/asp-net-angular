710310dd9995842a3f2c646e74c3fb26af04d04a
Ceccattini <Danny.Ceccattini@lendlease.com>
2019-06-07 16:19:53 +1000

    adding 3rd party image loader. Incomplete

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -9,6 +9,12 @@ import { Photo } from 'src/app/_models/photo';
 export class PhotoEditorComponent implements OnInit {
   @Input() photos: Photo[];
 
+  public uploader:FileUploader = new FileUploader({url: URL});
+  public hasBaseDropZoneOver:boolean = false;
+  public hasAnotherDropZoneOver:boolean = false;
+  public fileOverBase(e:any):void {
+  this.hasBaseDropZoneOver = e;
+
   constructor() { }
 
   ngOnInit() {
