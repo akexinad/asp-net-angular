56124593d4945623bd395a97b01c9faaf4b97fff
Ceccattini <Danny.Ceccattini@lendlease.com>
2019-06-11 09:10:04 +1000

    139. Implementing pagination in the API

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -45,11 +45,11 @@ namespace DatingApp.API.Data
             return user;
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
+            var users = _context.Users.Include(p => p.Photos);
 
-            return users;
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<bool> SaveAll()
