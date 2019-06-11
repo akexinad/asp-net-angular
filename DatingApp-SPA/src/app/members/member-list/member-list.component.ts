6acace04df98bff90996b3aaf2e5254b57ccd582
Ceccattini <Danny.Ceccattini@lendlease.com>
2019-06-11 10:06:22 +1000

    141. Using nix-bootstrap pagination module

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { User } from '../../_models/user';
+import { Pagination, PaginatedResult } from 'src/app/_models/pagination';
 
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-list',
@@ -13,6 +14,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
+  pagination: Pagination;
 
   constructor(
     private userService: UserService,
@@ -23,17 +25,24 @@ export class MemberListComponent implements OnInit {
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.users = data.users.result;
+      this.pagination = data.users.pagination;
     });
   }
 
-  // loadUsers() {
-  //   this.userService
-  //     .getUsers()
-  //     .subscribe((users: User[]) => {
-  //       this.users = users;
-  //   }, error => {
-  //     this.alertify.error(error);
-  //   });
-  // }
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService
+      .getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+      .subscribe((res: PaginatedResult<User[]>) => {
+        this.users = res.result;
+        this.pagination = res.pagination;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
 
 }
